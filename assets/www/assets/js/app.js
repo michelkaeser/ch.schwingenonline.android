138b6de3e9c8c5752248ea252f2040ba90f71da0
Michel Käser <mk@frontender.ch>
2013-05-28 01:39:26 +0200

    Fries prototyp

--- assets/www/assets/js/app.js
+++ assets/www/assets/js/app.js
@@ -1,44 +1,26 @@
-/*
- * onLoad() - loaded first on each page load
- *****************************************************************************/
+/**
+ * jQuery Document Ready Event.
+ *
+ * The jQuery document ready event is raised right after jQuery is fully loaded.
+ *
+ * @event ready
+ * @param {Event} $ raised event
+ */
+jQuery(document).ready(function($) {
 
-function onLoad() {
-	// add cordova eventListener for deviceReady
-    document.addEventListener('deviceready', onDeviceReady, false);
-
-    // initialize some global variables
-    backButtonAction = null;
-}
-
-
-/*
- * onDeviceReady() - fires by onLoad() as soon as cordova is ready
- *****************************************************************************/
-
-function onDeviceReady() {
-    // add various cardova eventListeners
-    document.addEventListener('backbutton', onBackKeyDown, false);
-    document.addEventListener('menubutton', onMenuKeyDown, false);
-    document.addEventListener('online', onOnline, false);
-    document.addEventListener('offline', onOffline, false);
-}
-
-
-/*
- * jQuery document ready
- *****************************************************************************/
-
- $(document).ready(function() {
-    // we hide default content until jQuery is ready
-    $('@modal-box--loading').hide();
-
-    // clickEvent for actiobar-menu--togglers
-    $('@actionbar-menu--toggler').click(function(e) {
-        var target = $(this).data('target');
-        $(this).toggleClass('active');
-        $('*[data-name="'+ target +'"]').toggle();
-
-        $(this).data('back', 'true');
-        backButtonAction = ".click()";
+    /**
+     * Event that gets raised every time a link is clicked.
+     *
+     * @event click
+     * @param {Event} e raised event
+     */
+    $('a').click(function(e) {
+        e.preventDefault();
     });
+
+    /*var data = { name: "Hello" }
+    var template = $('#view_home').html();
+    var html = Mustache.render(template, data);
+    $('#wrap').html(html);*/
+
 });
--- assets/www/assets/js/events.js
+++ assets/www/assets/js/events.js
@@ -1,36 +1,64 @@
-/*
- * onBackKeyDown() - fired each time backButton is pressed
- *****************************************************************************/
-
-function onBackKeyDown() {
-	// check if we can undo an action/event
-	if (backButtonAction != null) {
-		eval("$('*[data-back=\"true\"]')" + backButtonAction);
-
-		$('*[data-back=\"true\"]').data('back', 'false');
-		backButtonAction = null;
-	} else {
-		navigator.app.backHistory;
-	}
+/**
+ * Function/event that is fired as soon as the DOM is loaded.
+ *
+ * The onLoad function can be used to execute code as soon as the DOM is loaded.
+ * It needs to be triggered in the <body> by adding: onload="onLoad()"
+ *
+ * @method onLoad
+ */
+function onLoad() {
+    document.addEventListener('deviceready', onDeviceReady, false);
 }
 
+/**
+ * Function/event that is fired as soon as Cordova (PhoneGap) is ready.
+ *
+ * The Cordova framework can add various listeners to the application.
+ * We have to wait until it's loaded before we can add them though.
+ *
+ * @event onDeviceReady
+ */
+function onDeviceReady() {
+    document.addEventListener('backbutton', onBackKeyDown, false);
+    document.addEventListener('menubutton', onMenuKeyDown, false);
+    document.addEventListener('online', onOnline, false);
+    document.addEventListener('offline', onOffline, false);
+}
 
-/*
- * onMenuKeyDown() - fired each time menuButton is pressed
- *****************************************************************************/
+/**
+ * Event that gets raised after back button press.
+ *
+ * This event is raised by Cordova every time the back button press is detected.
+ *
+ * @event onBackKeyDown
+ */
+function onBackKeyDown() {
+	navigator.app.backHistory;
+}
 
+/**
+ * Event that gets raised after menu button press.
+ *
+ * This event is raised by Cordova every time the menu button press is detected.
+ *
+ * @event onMenuKeyDown
+ */
 function onMenuKeyDown() {}
 
-
-/*
- * onOnline() - fired each time the device is online/connected
- *****************************************************************************/
-
+/**
+ * Event that gets raised when the device goes online.
+ *
+ * This event is raised by Cordova every time the device changes it's network state to online.
+ *
+ * @event onOnline
+ */
 function onOnline() {}
 
-
-/*
- * onOffline() - fired each time the device is offline/disconnected
- *****************************************************************************/
-
+/**
+ * Event that gets raised when the device goes offline.
+ *
+ * This event is raised by Cordova every time the device changes it's network state to offline.
+ *
+ * @event onOnline
+ */
 function onOffline() {}
